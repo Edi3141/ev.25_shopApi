e4425ea2a891732689108397cd28274e30094d02
User <user@Users-MacBook-Pro.local>
2023-01-19 20:36:12 +0600

    product viewset with permissions done

--- account/views.py
+++ account/views.py
@@ -1,8 +1,10 @@
 
 from django.contrib.auth import get_user_model
 from rest_framework import permissions
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from . import serializers
 from .send_mail import send_confirmation_email
@@ -30,17 +32,32 @@ class RegistrationView(APIView):
             return Response(serializer.data, status=201)
         return Response('Bad request!', status=400)
 
+
 class ActivationView(APIView):
     permission_classes = (permissions.AllowAny,)
 
     def get(self, request, activation_code):
         try:
-            user = User.object.get(activation_code=activation_code)
+            user = User.objects.get(activation_code=activation_code)
             user.is_active = True
             user.activation_code = ''
             user.save()
             return Response({'msg': 'Successfully activated!'}, status=200)
         except User.DoesNotExist:
             return Response({'msg': 'Link expired!'}, status=400)
+
+
+class LoginView(TokenObtainPairView):
+    permission_classes = (permissions.AllowAny,)
+
+
+class LogoutView(GenericAPIView):
+    serializer_class = serializers.LogoutSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response('Successfully logged out!', status=200)
 
 
